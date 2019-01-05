4bea71d01f11a841c6e65b07862fec9d2f143f38
leandro monteiro de castro <leandrinho_mc22@hotmail.com>
2019-01-05 16:56:15 -0200

    Ajuste inicial

--- Frm_Inventário/Form1.Designer.vb
+++ Frm_Inventário/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class FrmInventario
     Inherits System.Windows.Forms.Form
 
     'Descartar substituições de formulário para limpar a lista de componentes.
@@ -22,10 +22,17 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'FrmInventario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Name = "FrmInventario"
+        Me.Text = "Inventário"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- Frm_Inventário/My Project/Application.Designer.vb
+++ Frm_Inventário/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Frm_Inventário.Form1
+            Me.MainForm = Global.Frm_Inventário.FrmInventario
         End Sub
     End Class
 End Namespace
